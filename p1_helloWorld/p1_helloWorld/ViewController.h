22e4735e8e40c3826b63691a0fc1d240cadc99a6
Jack Curtin <jcurtin4@binghamton.edu>
2021-02-19 11:28:54 -0500

    Add cat button and dog slider

--- p1_helloWorld/p1_helloWorld/ViewController.h
+++ p1_helloWorld/p1_helloWorld/ViewController.h
@@ -9,6 +9,15 @@
 
 @interface ViewController : UIViewController
 @property (nonatomic, strong) IBOutlet UILabel *myLabel;
+@property (nonatomic, strong) IBOutlet UIImageView *catLabel;
+@property (nonatomic, strong) IBOutlet UILabel *meowLabel;
+
+@property (nonatomic, strong) IBOutlet UIImageView *dogLabel;
+@property (nonatomic, strong) IBOutlet UISlider *dogSlider;
+
+
+
+
 
 
 @end
--- p1_helloWorld/p1_helloWorld/ViewController.m
+++ p1_helloWorld/p1_helloWorld/ViewController.m
@@ -13,13 +13,52 @@
 
 @implementation ViewController
 @synthesize myLabel;
+@synthesize catLabel;
+@synthesize meowLabel;
+@synthesize dogLabel;
+@synthesize dogSlider;
+
+
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    catLabel.image = [UIImage imageNamed:@""];
+    dogLabel.image = [UIImage imageNamed:@"dog2"];
+
 }
 
 - (IBAction)clickedTheButton:(id)sender
 {
     [myLabel setText:@"Button clicked"];
+}
+
+- (IBAction)selectButton:(id)sender
+{
+    if(catLabel.image == [UIImage imageNamed:@"catImage"] || catLabel.image == [UIImage imageNamed:@""]){
+        catLabel.image = [UIImage imageNamed:@"catImage2"];
+        [meowLabel setText:@"MEOW *loudly*"];
+
+    }
+    else{
+        catLabel.image = [UIImage imageNamed:@"catImage"];
+        [meowLabel setText:@"meow *softly*"];
+    }
+}
+- (IBAction)selectDog:(id)sender
+{
+    if(dogSlider.value <= .33){
+        dogLabel.image = [UIImage imageNamed:@"dog1"];
+
+    }
+    if(dogSlider.value >= .34 && dogSlider.value <= .65){
+        dogLabel.image = [UIImage imageNamed:@"dog2"];
+
+    }
+    if(dogSlider.value >= .66){
+        dogLabel.image = [UIImage imageNamed:@"dog3"];
+
+    }
 }
 @end
